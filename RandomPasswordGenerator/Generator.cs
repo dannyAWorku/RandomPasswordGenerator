43b04148d7a88f7865fc5784fb208a78f28195d8
dannyAWorku <danielaworku@gmail.com>
2024-07-22 17:45:17 +0300

    Add project files.

---
+++ RandomPasswordGenerator/Generator.cs
@@ -0,0 +1,32 @@
+﻿namespace RandomPasswordGenerator
+{
+    public class Generator
+    {
+        public int NumberOfPassword { get; set; }
+        public string[] Password { get; }
+        Random random = new Random();
+
+
+        public Generator( int numberOfPassword) 
+        { 
+            NumberOfPassword = numberOfPassword;
+            Password = new string[NumberOfPassword];
+            char[] SpecialChars = new char[] { '@','#','$','%','&','*'};            
+            for (int i = 0; i < NumberOfPassword; i++)
+            {
+                var Upper = char.ConvertFromUtf32(random.Next(65, 91));
+                var Lower1 = char.ConvertFromUtf32(random.Next(97, 122));
+                var Lower2 = char.ConvertFromUtf32(random.Next(97, 122));
+                char SpecialChar = SpecialChars[random.Next(0, 6)];
+                int FourDigit = random.Next(1000, 10000);
+                Password[i] = Upper + SpecialChar + FourDigit.ToString() + Lower1 + Lower2;
+            }
+        }
+
+        public string[] GetPasswords()
+        {
+            return Password;
+        }  
+
+    }
+}
---
+++ RandomPasswordGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RandomPasswordGenerator;
+
+Console.WriteLine("The Number of Passwords needed");
+
+int numberOfPasswords = Convert.ToInt32(Console.ReadLine());
+Generator generator =new Generator(numberOfPasswords);
+string[]  passwords = generator.GetPasswords();
+
+for (int i = 0; i < passwords.Length; i++)
+{
+    Console.WriteLine(passwords[i]);
+}
